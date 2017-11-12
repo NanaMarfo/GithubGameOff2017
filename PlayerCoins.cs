b31a85b3fe4c2e35f053654ec8cb35aa917d0eec
NanaMarfo <33296748+NanaMarfo@users.noreply.github.com>
2017-11-12 10:11:42 +0000

    Add files via upload

---
+++ PlayerCoins.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class PlayerCoins : MonoBehaviour
+{
+    public Text Coins;
+    public int CoinsMG = 0;
+
+    void Start()
+    {
+        CoinsMG = 0;
+        Coins.text = "CoinsMG: " + CoinsMG.ToString() ;
+    }
+
+
+    void Update()
+    {
+        Coins.text = "CoinsMG: " + CoinsMG.ToString();
+    }
+
+    void OnTriggerEnter(Collider Cinfo)
+    {
+        if (Cinfo.tag == "MG")
+        {
+            CoinsMG = CoinsMG + 1;
+        }
+    }
+}
